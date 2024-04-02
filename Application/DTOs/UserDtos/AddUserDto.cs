cc2176e016cd92753d80bc917eabe0b9932d4e04
Temurbek <ubaydullayev117@gmail.com>
2024-04-02 15:23:08 +0500

    Add project files.

---
+++ Application/DTOs/UserDtos/AddUserDto.cs
@@ -0,0 +1,27 @@
+﻿using Domain.Entities;
+using Domain.Enums;
+
+namespace Application.DTOs.UserDtos;
+public class AddUserDto
+{
+    public string FirstName { get; set; } = "";
+    public string LastName { get; set; } = "";
+    public string Email { get; set; } = "";
+    public Gender Gender { get; set; }
+    public string Password { get; set; } = "";
+    public Role Role { get; set; }
+
+    public static implicit operator User(AddUserDto dto)
+    {
+        return new User 
+        {
+            FirstName = dto.FirstName,
+            LastName = dto.LastName,
+            Email = dto.Email,
+            Gender = dto.Gender,
+            Password = dto.Password,
+            Role = dto.Role,
+        };
+
+    }
+}
